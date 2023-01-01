ff0a5e08893dd265014e814b41456ae8c3dbabc0
Joe Irvine <jirvine124@gmail.com>
2022-12-31 21:44:27 -0700

    inital commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+	let value = '';
+	const re = new RegExp(/#/g);
+	function format() {
+		console.log(value);
+		value = value.replaceAll(re, '\n# ');
+	}
+</script>
+
+<h1>W-Tools</h1>
+<form>
+	<fieldset>
+		<legend>Format</legend>
+		<textarea name="textarea" rows="50" cols="100" bind:value placeholder="Format Log Here" />
+		<p><button on:click={format}>Format</button></p>
+	</fieldset>
+</form>
