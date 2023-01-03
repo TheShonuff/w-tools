0347c5aea10c93b03a7ced5fbfb729dbba21e1aa
Joe Irvine <jirvine124@gmail.com>
2023-01-03 01:36:16 -0700

    add copy to clipboard and clear button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,16 @@
 	function format() {
 		console.log(value);
 		value = value.replaceAll(re, '\n# ');
+		navigator.clipboard.writeText(value).then(() => copied());
+	}
+	function copied() {
+		let x = document.querySelector('.copied');
+		let legend = document.querySelector('legend');
+		legend.innerHTML = 'COPIED';
+		x.innerHTML = "LOOK AT THAT!!! It's been copied to your clipboard";
+	}
+	function clear() {
+		value = '';
 	}
 </script>
 
@@ -12,6 +22,22 @@
 	<fieldset>
 		<legend>Format</legend>
 		<textarea name="textarea" rows="50" cols="100" bind:value placeholder="Format Log Here" />
-		<p><button on:click={format}>Format</button></p>
+		<p>
+			<button class="format-btn" on:click={format}>Format</button><button
+				class="clear-btn"
+				on:click={clear}>Clear</button
+			>
+		</p>
+		<p class="copied" />
 	</fieldset>
 </form>
+
+<style>
+	.copied {
+		font-weight: bold;
+	}
+
+	.format-btn {
+		margin-right: 10px;
+	}
+</style>
